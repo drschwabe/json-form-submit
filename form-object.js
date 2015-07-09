5726457abafdf894f07b96b943f2aa8fd809ca56
Derrick Schwabe <admin@mrschwabe.com>
2015-07-08 22:25:50 -0600

    Use "form-serialize" to do the form to object conversion.

--- form-object.js
+++ form-object.js
@@ -1,22 +1,20 @@
 var $ = require('jquery'), 
-    _ = require('underscore')
+    _ = require('underscore'), 
+    serialize = require('form-serialize')
 
 module.exports = function(form, callback) {
 
   //If the first argument is a function...
   if(_.isFunction(form)) {
     callback = form
-    form = $('form') //< capture all forms.
+    form = $('form') //< If none specified, assume just the one form.
   }
 
   //Form handler: 
   $(form).submit(function(event) {
     event.preventDefault() //< Prevent page reload as per default HTML submit behavior.
     //Convert the form to a tidy little object: 
-    var formObj = {}  
-    $(form).serializeArray().forEach(function(element) {
-      formObj[element.name] = element.value
-    })
+    var formObj = serialize(form[0], { hash: true })
     callback(formObj)
   })
 }
