11bb33f68a73c65131826bb91b703082e29f9183
Derrick Schwabe <admin@mrschwabe.com>
2015-07-13 14:03:59 -0600

    Support form id argument without hash.

--- json-form-submit.js
+++ json-form-submit.js
@@ -8,13 +8,17 @@ module.exports = function(form, callback) {
   if(_.isFunction(form)) {
     callback = form
     form = $('form') //< If none specified, assume just the one form.
+  //Otherwise it's a string..
+  } else if( form.substr(0, 1) != '#' ) {
+    //if there's no hash on the form ID add it now:      
+    form = '#' + form
   }
 
   //Form handler: 
   $(form).submit(function(event) {
     event.preventDefault() //< Prevent page reload as per default HTML submit behavior.
     //Convert the form to a tidy little object: 
-    var formObj = serialize(form[0], { hash: true })
+    var formObj = serialize($(form)[0], { hash: true })
     callback(formObj)
   })
 }
