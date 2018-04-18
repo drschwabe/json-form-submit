c7b37ed8efe47ab1cc6790cc1d5f71be13dc499c
Jiafeng Wu <jiafengwu0301@gmail.com>
2018-04-18 16:02:53 -0600

    remove jquery and underscore dependency

--- json-form-submit.js
+++ json-form-submit.js
@@ -1,35 +1,40 @@
-var $ = require('jquery'),
-    _ = require('underscore'),
-    serialize = require('form-serialize')
+var serialize = require('form-serialize')
 
 module.exports = function(param1, callback) {
 
   var jform = {}
-
-  if(_.isString(param1)) jform.form_id = param1 //< String supplied.
+  if(typeof param1 == 'string') jform.form_id = param1 //< String supplied.
   else jform = param1 //< Object supplied
 
   //Form handler:
-  $('body').on('submit', '#' + jform.form_id, function(event) {
+  document.getElementById(jform.form_id).addEventListener("submit", function(event){
+  // $('body').on('submit', '#' + jform.form_id, function(event) {
     event.preventDefault() //< Prevent page reload as per default HTML submit behavior.
     //Convert the form to a tidy little object:
-    var formObj = serialize($('#' + jform.form_id)[0], { hash: true })
+    var formObj = serialize(document.getElementById(jform.form_id), { hash: true })
 
     if(jform.pre_post) {
       //Call the pre_post function and update the formObj's value to the return
       //value of said function.
       var prePostFormObj = jform.pre_post(formObj)
-      //Only update the formObj if a return value was provided: 
+      //Only update the formObj if a return value was provided:
       if(prePostFormObj) formObj = prePostFormObj
     }
 
     if(jform.post_url) {
-      $.post(jform.post_url, formObj, function(res) {
-        return callback(formObj, res)
-      }, 'json')
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", jform.post_url, true);
+      xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+        }
+      }
+      var data = JSON.stringify(formObj);
+      xhr.send(data);
     }
 
-    //Else: 
+    //Else:
     return callback(formObj)
   })
 }
